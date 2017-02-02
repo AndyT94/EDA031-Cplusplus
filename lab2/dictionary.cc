56f9c02e68c94db22c69df160322662d545c0caa
AndyT94 <andytruong94@gmail.com>
2017-02-02 17:47:00 +0100

    Exists in directory done

--- lab2/dictionary.cc
+++ lab2/dictionary.cc
@@ -9,10 +9,22 @@
 using namespace std;
 
 Dictionary::Dictionary() {
+	ifstream input("words.txt");
+	string line;
+
+	if (input.is_open()) {
+		while (getline(input,line)) {
+			size_t firstDigit = line.find_first_of("0123456789");
+			dict.insert(line.substr(0, firstDigit - 1));
+		}
+		input.close();
+	} else {
+		cout << "ERROR: Unable to open file(s)";
+	}
 }
 
 bool Dictionary::contains(const string& word) const {
-	return true;
+	return dict.count(word) > 0;
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
