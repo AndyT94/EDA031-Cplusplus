4c033127a0804ac73ce7a6a10cb924b524c8448d
AndyT94 <andytruong94@gmail.com>
2017-02-02 18:17:46 +0100

    Words implemented

--- lab2/word.cc
+++ lab2/word.cc
@@ -1,15 +1,32 @@
-#include <string>
+ #include <string>
 #include <vector>
 #include "word.h"
 
 using namespace std;
 
-Word::Word(const string& w, const vector<string>& t) {}
+Word::Word(const string& w, const vector<string>& t) : word(w), trigrams(t) {}
 
 string Word::get_word() const {
-	return string();
+	return word;
 }
 
 unsigned int Word::get_matches(const vector<string>& t) const {
+	int matches = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while(i < trigrams.size() && j < t.size()) {
+		int comparison = trigrams[i].compare(t[j]);
+		if(comparison < 0) {
+			++i;
+		} else if (comparison > 0) {
+			++j;
+		} else {
+			++matches;
+			++i;
+			++j;
+		}
+	}
+
 	return 0;
 }
