22d8462364b4a78545d8abe610d2a6b260955bfd
Andy Truong <dat13at1@hacke-12.student.lth.se>
2017-02-10 10:31:15 +0100

    A3 done

--- lab3/date.cc
+++ lab3/date.cc
@@ -1,5 +1,11 @@
 #include <ctime>  // time and localtime
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <regex>
 #include "date.h"
+
+using namespace std;
 
 int Date::daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
@@ -11,7 +17,11 @@ Date::Date() {
 	day = locTime->tm_mday;
 }
 
-Date::Date(int y, int m, int d) {}
+Date::Date(int y, int m, int d) {
+	year = y;
+	month = m;
+	day = d;
+}
 
 int Date::getYear() const {
 	return year;
@@ -26,7 +36,7 @@ int Date::getDay() const {
 }
 
 void Date::next() {
-	if(day + 1 > daysPerMonth[month]) {
+	if(day + 1 > daysPerMonth[month - 1]) {
 		day = 1;
 
 		if(month + 1 > 12) {
@@ -39,4 +49,36 @@ void Date::next() {
 	} else {
 		day += 1;
 	}
+}
+
+istream& operator>>(istream& is, Date& date) {
+	string line;
+	getline(is, line);
+	line = regex_replace(line, regex("\\s*"), "");
+
+	size_t first = line.find("-");
+	size_t second = line.rfind("-");
+
+	string y = line.substr(0, first);
+	string m = line.substr(first + 1, second - first - 1);
+	string d = line.substr(second + 1);
+
+	int i_y = stoi(y);
+	int i_m = stoi(m);
+	int i_d = stoi(d);
+
+	if(i_y > 0 && i_m > 0 && i_m < 13 && i_d > 0 && i_d <= date.daysPerMonth[i_m - 1]) {
+		date.year = i_y;
+		date.month = i_m;
+		date.day = i_d;
+	} else {
+		is.setstate(ios_base::failbit);
+	}
+
+	return is;
+}
+
+ostream& operator<<(ostream& os, Date& date) {
+	os << date.year << "-" << setfill('0') << setw(2) << date.month << "-" << setfill('0') << setw(2) << date.day << endl;
+	return os;
 }
--- lab3/date.h
+++ lab3/date.h
@@ -1,6 +1,8 @@
 #ifndef DATE_H
 #define DATE_H
 
+#include <iostream>
+
 class Date {
 public:
 	Date();                    // today's date
@@ -9,11 +11,16 @@ public:
 	int getMonth() const;      // get the month
 	int getDay() const;        // get the day
 	void next();               // advance to next day
+	friend std::istream& operator>>(std::istream& is, Date& date);
+	friend std::ostream& operator<<(std::ostream& is, Date& date);
 private:
 	int year;  // the year (four digits)
 	int month; // the month (1-12)
 	int day;   // the day (1-..)
 	static int daysPerMonth[12]; // number of days in each month
 };
+
+std::istream& operator>>(std::istream& is, Date& date);
+std::ostream& operator<<(std::ostream& is, Date& date);
 
 #endif
