9aa4408ad067261308709888f7d9bea8b3372950
AndyT94 <andytruong94@gmail.com>
2017-02-11 20:41:07 +0100

    A5 done

--- lab3/stringcast.cc
+++ lab3/stringcast.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 
 using namespace std;
 
@@ -9,8 +10,10 @@ template <typename T>
 inline T string_cast(const string& s)  {
     stringstream convert(s);
     T value;
-    convert >> value;
-    return value;
+    if(convert >> value) {
+      return value;
+    }
+    throw invalid_argument("Invalid argument");
 }
 
 int main() {
@@ -18,7 +21,6 @@ int main() {
     int i = string_cast<int>("123");
     double d = string_cast<double>("12.34");
     Date date = string_cast<Date>("2015-01-10");
-    Date ddwa = string_cast<Date>("2015-01-105454");
   } catch (invalid_argument& e) {
     cout << "Error: " << e.what() << endl;
   }
